a13374bcdb90a57ef10c4e9d052de8b40a21f745
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-06-20 14:48:15 +0530

    learn

--- js/day 10/arrowfn.js
+++ js/day 10/arrowfn.js
@@ -1,33 +1,33 @@
-// // Regular fn expression 
+// Regular fn expression 
 
-// const regularFunction = function (param){
-//     return isExpression;
-// };
+const regularFunction = function (param){
+    return isExpression;
+};
 
-// //arrow function 
-// const arrowFunction = (param) => expression ;
-
-
-// const gfg = () => {
-//     console.log("Hi arun");
-// }
-
-// gfg()
+//arrow function 
+const arrowFunction = (param) => expression ;
 
 
-// const gfg = (x, y , z) =>{
-//     console.log(x + y + z);
-// }
+const gfg = () => {
+    console.log("Hi arun");
+}
 
-// gfg (10,20,30)
+gfg()
+
+
+const gfg = (x, y , z) =>{
+    console.log(x + y + z);
+}
+
+gfg (10,20,30)
 
 
 
-// const gfg = (x, y , z=30)=> {
-//     console.log(x +" "+ y +" "+ z);
-// }
+const gfg = (x, y , z=30)=> {
+    console.log(x +" "+ y +" "+ z);
+}
 
-// gfg (10,20)
+gfg (10,20)
 
 
 async parm => expression 
